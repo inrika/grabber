fd2239e858f3ceeabc42151cd63ebb8e6300ed4a
vagrant <vagrant@precise32.(none)>
2017-02-06 17:44:21 +0400

    фильтр

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,14 +7,13 @@ class AccountsController < ApplicationController
     @account = Account.find_or_initialize_by(email: account_params[:email])
     @account.assign_attributes(account_params)
     if @account.save
-#      gmail = Gmail.connect(@account.email, @account.password)
-#      if gmail.logged_in?
-       gmail = "gmail"
+      gmail = Gmail.connect(@account.email, @account.password)
+      if gmail.logged_in?
         @account.create_letters(gmail,params[:startDate], params[:endDate])
         redirect_to letters_path
-#      else
-#        redirect_to new_account_path, notice: 'нет соединения'
-#      end
+      else
+        redirect_to new_account_path, notice: 'нет соединения'
+      end
     else
       redirect_to new_account_path, notice: 'не записалось'
     end
--- app/models/account.rb
+++ app/models/account.rb
@@ -9,23 +9,20 @@ class Account < ApplicationRecord
 
   def create_letters(gmail,startDate,endDate)
     current_date = Time.now
-#    gmail.inbox.emails( :after => startDate,:before => endDate).each do |email|
-#      letters.create(text:email.text_part.decoded, date: email.date, is_read: email.read?, files_count:email.attachments.count, timestamp: current_date)
-#    end
-#    gmail.logout
+    gmail.inbox.emails( :after => startDate,:before => endDate).each do |email|
+      letters.create(text:email.text_part.decoded, date: email.date, is_read: email.read?, files_count:email.attachments.count, timestamp: current_date)
+    end
+    gmail.logout
 
-# без почты
-
-
-
-  letters.create(text:"письмо1", date: Time.new(2017, 01, 24), is_read: false, files_count:0, timestamp: current_date)
-  letters.create(text:"письмо2", date: Time.new(2017, 01, 25), is_read: true, files_count:1, timestamp: current_date)
-  letters.create(text:"письмо3", date: Time.new(2017, 01, 25), is_read: true, files_count:3, timestamp: current_date)
-  letters.create(text:"письмо4", date: Time.new(2017, 01, 25), is_read: false, files_count:5, timestamp: current_date)
-  letters.create(text:"письмо5", date: Time.new(2017, 01, 27), is_read: true, files_count:0, timestamp: current_date)
-  letters.create(text:"письмо6", date: Time.new(2017, 01, 29), is_read: true, files_count:5, timestamp: current_date)
-  letters.create(text:"письмо7", date: Time.new(2017, 01, 30), is_read: true, files_count:10, timestamp: current_date)
-  letters.create(text:"письмо8", date: Time.new(2017, 01, 30), is_read: true, files_count:5, timestamp: current_date)
+# без подключения к gmail
+#  letters.create(text:"письмо1", date: Time.new(2017, 01, 24), is_read: false, files_count:0, timestamp: current_date)
+#  letters.create(text:"письмо2", date: Time.new(2017, 01, 25), is_read: true, files_count:1, timestamp: current_date)
+#  letters.create(text:"письмо3", date: Time.new(2017, 01, 25), is_read: true, files_count:3, timestamp: current_date)
+#  letters.create(text:"письмо4", date: Time.new(2017, 01, 25), is_read: false, files_count:5, timestamp: current_date)
+#  letters.create(text:"письмо5", date: Time.new(2017, 01, 27), is_read: true, files_count:0, timestamp: current_date)
+#  letters.create(text:"письмо6", date: Time.new(2017, 01, 29), is_read: true, files_count:5, timestamp: current_date)
+#  letters.create(text:"письмо7", date: Time.new(2017, 01, 30), is_read: true, files_count:10, timestamp: current_date)
+#  letters.create(text:"письмо8", date: Time.new(2017, 01, 30), is_read: true, files_count:5, timestamp: current_date)
 
   end
 
